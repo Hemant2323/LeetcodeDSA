31cd2f0ef8273238421483146ffb24e2bf744da0
Hemant2323 <hemant27goyal@gmail.com>
2026-07-02 06:42:26 +0530

    Time: 387 ms (51.38%) | Memory: 211.5 MB (45.43%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    int f(int ind, int buy, vector<int>& prices, int n, vector<vector<vector<int>>> &dp,
-          int cap) {
+    int f(int ind, int buy, vector<int>& prices, int n,
+          vector<vector<vector<int>>>& dp, int cap) {
         if (ind == n) {
             return 0;
         }
@@ -24,8 +24,31 @@ public:
     }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp( n, vector<vector<int>>(2, vector<int>(3, -1)));
-        int cap = 2;
-        return f(0, 1, prices, n, dp, cap);
+        vector<vector<vector<int>>> dp(
+            n+1, vector<vector<int>>(2, vector<int>(3, -1)));
+        // int cap = 2;
+        // return f(0, 1, prices, n, dp, cap);
+        dp[0][1][0]=0;
+        dp[0][0][0]=0;
+
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                for (int cap = 2; cap >= 1; cap--) {
+                    if (buy) {
+                        int not_take = dp[ind+1][1][cap];
+                        int take =
+                            -prices[ind] + dp[ind+1][0][cap];
+
+                        dp[ind][buy][cap] = max(take, not_take);
+                    } else {
+                        int sell =
+                            prices[ind] + dp[ind+1][1][cap-1];
+                        int not_sell = dp[ind+1][0][cap];
+                         dp[ind][buy][cap] = max(sell, not_sell);
+                    }
+                }
+            }
+        }
+        return dp[0][1][2]+1;
     }
 };
