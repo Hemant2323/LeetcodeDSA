5a71479b561e71d4cbc1276049bc8f2a6f0282f5
Hemant2323 <hemant27goyal@gmail.com>
2026-07-02 05:57:12 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (86.50%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-int f(int ind, int buy, vector<int>& prices, int n,
+    int f(int ind, int buy, vector<int>& prices, int n,
           vector<vector<int>>& dp) {
         if (ind >= n) {
             return 0;
@@ -10,7 +10,7 @@ int f(int ind, int buy, vector<int>& prices, int n,
         }
         if (buy) {
             int not_take = f(ind + 1, 1, prices, n, dp);
-            int take = -prices[ind] + f(ind+1, 0, prices, n, dp);
+            int take = -prices[ind] + f(ind + 1, 0, prices, n, dp);
 
             return dp[ind][buy] = max(take, not_take);
         } else {
@@ -20,9 +20,37 @@ int f(int ind, int buy, vector<int>& prices, int n,
         }
     }
     int maxProfit(vector<int>& prices) {
-         int n = prices.size();
-         vector<vector<int>> dp(n + 1, vector<int>(2, -1));
+        int n = prices.size();
+        //vector<vector<int>> dp(n + 1, vector<int>(2, -1));
 
-         return f(0,1,prices,n,dp);
+        // return f(0,1,prices,n,dp);
+        vector<int> ahead2(2,0);
+        vector<int> ahead1(2,0);
+        vector<int> curr(2,0);
+
+        for (int ind = n - 1; ind >= 0; ind--) {
+
+            for (int buy = 0; buy <= 1; buy++) {
+
+                if (buy) {
+
+                    int take = -prices[ind] + ahead1[0];
+                    int notTake = ahead1[1];
+
+                    curr[buy] = max(take, notTake);
+
+                } else {
+
+                    int sell = prices[ind] + ahead2[1];
+                    int notSell = ahead1[0];
+
+                    curr[buy] = max(sell, notSell);
+                }
+                ahead2=ahead1;
+                ahead1=curr;
+            }
+        }
+
+        return ahead1[1];
     }
 };
