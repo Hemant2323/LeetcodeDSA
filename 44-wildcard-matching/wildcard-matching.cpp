5677b20b26eebc69d15a74dae942158d8706c2fa
Hemant2323 <hemant27goyal@gmail.com>
2026-07-09 12:19:29 +0530

    Time: 58 ms (40.09%) | Memory: 15.6 MB (72.71%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,41 +1,74 @@
 class Solution {
 public:
-    bool f(int i,int j, string &s, string &p,vector<vector<int>>&dp){
-        if(i<0 && j<0){
+    bool f(int i, int j, string& s, string& p, vector<vector<int>>& dp) {
+        if (i < 0 && j < 0) {
             return true;
         }
-        
-        if(j>=0 && i<0){
-            for(int k=0;k<=j;k++){
-                if(p[k]!='*'){
+
+        if (j >= 0 && i < 0) {
+            for (int k = 0; k <= j; k++) {
+                if (p[k] != '*') {
                     return false;
                 }
             }
-           return true;
-           
+            return true;
         }
-        if(i>=0 && j<0){
+        if (i >= 0 && j < 0) {
             return false;
-            
         }
-        if(dp[i][j]!=-1){
+        if (dp[i][j] != -1) {
             return dp[i][j];
         }
 
-        if(s[i]==p[j]  || p[j]=='?'){
-            return dp[i][j]= f(i-1,j-1,s,p,dp);
+        if (s[i] == p[j] || p[j] == '?') {
+            return dp[i][j] = f(i - 1, j - 1, s, p, dp);
         }
-        if(p[j]=='*'){
-            return dp[i][j]= f(i-1,j,s,p,dp) || f(i,j-1,s,p,dp);
+        if (p[j] == '*') {
+            return dp[i][j] = f(i - 1, j, s, p, dp) || f(i, j - 1, s, p, dp);
         }
 
         return false;
     }
     bool isMatch(string s, string p) {
-        int i=s.size();
-        int j=p.size();
-        vector<vector<int>>dp(i+1,vector<int>(j+1,-1));
-        return f(i-1,j-1,s,p,dp);
-        
+        int n = s.size();
+        int m = p.size();
+        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, -1));
+        // return f(i-1,j-1,s,p,dp);
+
+        dp[0][0]=true;
+        for(int i=1;i<=n;i++){
+            dp[i][0]=false;
+
+        }
+
+        for(int j=1;j<=m;j++){
+            bool flag=true;
+
+            for(int k=1;k<=j;k++){
+                if(p[k-1]!='*'){
+                    flag=false;
+                    break;
+                }
+
+                
+            }
+            dp[0][j]=flag;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s[i-1] == p[j-1] || p[j-1] == '?') {
+                     dp[i][j] = dp[i-1][j-1];
+                }
+                else if (p[j-1] == '*') {
+                     dp[i][j] =
+                               dp[i-1][j] || dp[i][j-1];
+                }
+                else{
+                    dp[i][j]= false;
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
