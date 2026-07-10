54e4d4aee9c293e8860e7c032d89b9724aece882
Hemant2323 <hemant27goyal@gmail.com>
2026-07-10 08:46:03 +0530

    Time: 63 ms (61.32%) | Memory: 14.4 MB (48.22%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -16,29 +16,45 @@ public:
 
         return dp[ind][prev_ind + 1] = max(len, len1);
     }
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
+    int lengthOfLIS(vector<int>& arr) {
+        //int n = nums.size();
        // vector<vector<int>> dp(n+1, vector<int>(n + 1, 0));
         // return f(0,-1,nums,dp);
-        vector<int> curr(n+1,0),next(n+1,0);
+        // vector<int> curr(n+1,0),next(n+1,0);
 
-        for (int ind = n - 1; ind >= 0; ind--) {
-            for (int prev_ind = ind - 1; prev_ind >= -1; prev_ind--) {
-                int len = 0;
+        // for (int ind = n - 1; ind >= 0; ind--) {
+        //     for (int prev_ind = ind - 1; prev_ind >= -1; prev_ind--) {
+        //         int len = 0;
 
-                if (ind <n) {
-                    len = next[prev_ind+1];
-                    int len1 = 0;
-                    if (prev_ind == -1 || nums[ind] > nums[prev_ind]) {
-                        len1 = 1 + next[ind+1];
-                    }
+        //         if (ind <n) {
+        //             len = next[prev_ind+1];
+        //             int len1 = 0;
+        //             if (prev_ind == -1 || nums[ind] > nums[prev_ind]) {
+        //                 len1 = 1 + next[ind+1];
+        //             }
 
-                    curr[prev_ind + 1] = max(len, len1);
+        //             curr[prev_ind + 1] = max(len, len1);
+        //         }
+        //     }
+        //     next=curr;
+        // }
+
+        // return next[0];
+
+
+        int n = arr.size();
+      
+        vector<int> dp(n,1);
+        int maxi=1;
+        for(int i=0;i<n;i++){
+            for(int prev=0;prev<i;prev++){
+                if(arr[prev]<arr[i] ){
+                    dp[i]=max(dp[i],1+dp[prev]);
                 }
             }
-            next=curr;
+            maxi=max(maxi,dp[i]);
         }
 
-        return next[0];
+        return maxi;
     }
 };
