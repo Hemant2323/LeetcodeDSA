693f5c88d10fb5137e3b49883f87a032f334c94d
Hemant2323 <hemant27goyal@gmail.com>
2026-07-12 15:26:08 +0530

    Time: 1892 ms (8.93%) | Memory: 640.3 MB (20.61%) - LeetSync

---
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool compare(string s, string t) {
+        if (t.size() != s.size() + 1) {
+            return false;
+        }
+
+        int i = 0, j = 0;
+
+        int count = 0;
+        while (i < s.size() && j < t.size()) {
+            if (s[i] == t[j]) {
+                i++;
+                j++;
+            } else {
+                j++;
+                
+            }
+        }
+        if(i!=s.size()){
+            return false;
+        }
+        
+        return true;
+    }
+    int longestStrChain(vector<string>& words) {
+        sort(words.begin(), words.end(),
+             [](string& a, string& b) { return a.size() < b.size(); });
+        int n = words.size();
+
+        vector<int> dp(n, 1);
+        int maxi = 1;
+        for (int i = 0; i < n; i++) {
+            for (int prev = 0; prev < i; prev++) {
+
+                if (compare(words[prev], words[i])) {
+                    dp[i] = max(dp[i], 1 + dp[prev]);
+                }
+            }
+            maxi = max(maxi, dp[i]);
+        }
+
+        return maxi;
+    }
+};
