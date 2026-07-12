fed28664416fc4cbbcad5509c5115a09046b8782
Hemant2323 <hemant27goyal@gmail.com>
2026-07-12 15:29:22 +0530

    Time: 39 ms (77.15%) | Memory: 19 MB (55.54%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.cpp
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     bool compare(string s, string t) {
-        if (t.size() != s.size() + 1) {
-            return false;
-        }
+        // if (t.size() != s.size() + 1) {
+        //     return false;
+        // }
 
         int i = 0, j = 0;
 
@@ -14,13 +14,12 @@ public:
                 j++;
             } else {
                 j++;
-                
             }
         }
-        if(i!=s.size()){
+        if (i != s.size()) {
             return false;
         }
-        
+
         return true;
     }
     int longestStrChain(vector<string>& words) {
@@ -32,6 +31,9 @@ public:
         int maxi = 1;
         for (int i = 0; i < n; i++) {
             for (int prev = 0; prev < i; prev++) {
+                if (words[i].size() != words[prev].size() + 1) {
+                    continue;
+                }
 
                 if (compare(words[prev], words[i])) {
                     dp[i] = max(dp[i], 1 + dp[prev]);
