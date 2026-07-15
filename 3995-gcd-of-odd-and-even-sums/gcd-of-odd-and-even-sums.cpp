82447c4b3ce334932955e1ad22127134edc3c06e
Hemant2323 <hemant27goyal@gmail.com>
2026-07-16 01:06:56 +0530

    Time: 285 ms (11.22%) | Memory: 8.5 MB (85.03%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -7,7 +7,7 @@ public:
         int minn=min(even,odd);
         int maxx=1;
 
-        for(int i=1;i<=minn;i++){
+        for(int i=1;i<=minn/2;i++){
             if(even%i==0 && odd%i==0){
                 maxx=max(maxx,i);
             }
