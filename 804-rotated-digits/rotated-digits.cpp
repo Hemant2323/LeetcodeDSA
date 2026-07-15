767002a9c8d9a424f472be5bb783f8953a8567f5
Hemant2323 <hemant27goyal@gmail.com>
2026-07-16 01:54:48 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (64.47%) - LeetSync

--- 804-rotated-digits/rotated-digits.cpp
+++ 804-rotated-digits/rotated-digits.cpp
@@ -11,27 +11,41 @@ public:
         } else {
             for (int i = 0; i <= n; i++) {
                 int x = i;
-                vector<int> r;
+               // vector<int> r;
+                 bool invalid = false, change = false;
                 while (x > 0) {
                     int rem = x % 10;
-                    r.push_back(rem);
-
-                    x /= 10;
-                }
-                bool invalid = false, change = false;
-                for (int j = 0; j < r.size(); j++) {
-                    if (r[j] == 2 || r[j] == 5 || r[j] == 6 || r[j] == 9) {
+                   
+                    //  r.push_back(rem);
+                    if (rem == 2 || rem == 5 || rem == 6 || rem == 9) {
                         change = true;
                     }
-                    if (r[j] == 3 || r[j] == 4 || r[j] == 7) {
+                    if (rem == 3 || rem == 4 || rem == 7) {
                         invalid = true;
                         break;
                     }
-                }
 
-                if(!invalid && change){
-                    count++;
+                   
+
+                    x /= 10;
                 }
+                 if (!invalid && change) {
+                        count++;
+                    }
+                // bool invalid = false, change = false;
+                // for (int j = 0; j < r.size(); j++) {
+                //     if (r[j] == 2 || r[j] == 5 || r[j] == 6 || r[j] == 9) {
+                //         change = true;
+                //     }
+                //     if (r[j] == 3 || r[j] == 4 || r[j] == 7) {
+                //         invalid = true;
+                //         break;
+                //     }
+                // }
+
+                // if(!invalid && change){
+                //     count++;
+                // }
             }
         }
         return count;
