9975f070407f05f1a35e57b5010404d0b869c778
Hemant2323 <hemant27goyal@gmail.com>
2026-07-18 23:13:08 +0530

    Time: 1270 ms (51.11%) | Memory: 9.3 MB (87.06%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -1,39 +1,51 @@
 class Solution {
 public:
-   bool ispalindrome(int i,int j,string& s){
-        
-        while(i<j){
-            if(s[i]==s[j]){
+    bool ispalindrome(int i, int j, string& s) {
+
+        while (i < j) {
+            if (s[i] == s[j]) {
                 i++;
                 j--;
-            }
-            else{
+            } else {
                 return false;
             }
         }
         return true;
     }
-    int f(int i,string &s,vector<int>&dp ){
-        if(i==s.size()){
+    int f(int i, string& s, vector<int>& dp) {
+        if (i == s.size()) {
             return 0;
         }
-        if(dp[i]!=-1){
+        if (dp[i] != -1) {
             return dp[i];
         }
-        
-      int  mincost=1e9;
-        for(int j=i;j<s.size();j++){
-           
-            if(ispalindrome(i,j,s)){
-                int cost=1+f(j+1,s,dp);
-                mincost=min(mincost,cost);
+
+        int mincost = 1e9;
+        for (int j = i; j < s.size(); j++) {
+
+            if (ispalindrome(i, j, s)) {
+                int cost = 1 + f(j + 1, s, dp);
+                mincost = min(mincost, cost);
             }
         }
-        return dp[i]= mincost;
+        return dp[i] = mincost;
     }
     int minCut(string s) {
-        int n=s.size();
-        vector<int> dp(n,-1);
-        return f(0,s,dp)-1;
+        int n = s.size();
+        vector<int> dp(n + 1, 0);
+        // return f(0,s,dp)-1;
+       
+        for (int i = n - 1; i >= 0; i--) {
+             int mincost = 1e9;
+            for (int j = i; j < s.size(); j++) {
+
+                if (ispalindrome(i, j, s)) {
+                    int cost = 1 + dp[j + 1];
+                    mincost = min(mincost, cost);
+                }
+            }
+            dp[i]=mincost;
+        }
+        return dp[0]-1;
     }
 };
