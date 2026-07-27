d6a580802940f1ccdc523bc5025115692c0f7678
Hemant2323 <hemant27goyal@gmail.com>
2026-07-28 05:06:37 +0530

    Time: 13 ms (31.91%) | Memory: 7.7 MB (65.51%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,8 +1,12 @@
 class Solution {
 public:
+    int f(int n){
+        if(n<=1){
+            return n;
+        }
+        return f(n-1)+f(n-2);
+    }
     int fib(int n) {
-        if(n <= 1)return n;
- 
-        return fib(n-1) + fib(n-2);
+        return f(n);
     }
 };
