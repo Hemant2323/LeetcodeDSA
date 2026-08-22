d0120ed7708f649297c19b3f6212001037b1f0b4
Hemant2323 <hemant27goyal@gmail.com>
2026-08-22 11:39:11 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (5.78%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,28 +1,36 @@
 class Solution {
 public:
     int f(int i,int j,vector<int> & nums,vector<vector<int>>& dp){
-        
-            if(i==j){
-                return nums[i];
-            }
-            if(dp[i][j]!=-1){
-                return dp[i][j];
-            }
-            int pick=0,not_pick=0;
-             pick=nums[i]-f(i+1,j,nums,dp);
-             not_pick=nums[j]-f(i,j-1,nums,dp);
+       if(i==j){
+        return nums[i];
+       }
+       if(dp[i][j]!=-1){
+        return dp[i][j];
+       }
 
-            
-        
-        return dp[i][j]= max(pick,not_pick);
+       int leftpick=0,rightpick=0;
+       leftpick=nums[i]-f(i+1,j,nums,dp);
+       rightpick=nums[j]-f(i,j-1,nums,dp);
+
+       return dp[i][j]=max(leftpick,rightpick);
     }
     bool predictTheWinner(vector<int>& nums) {
+
+       
+
+
         int n=nums.size();
         vector<vector<int>> dp (n,vector<int>(n,-1));
-        int dif=f(0,n-1,nums,dp);
-        if(dif>=0){
+        int diff=f(0,n-1,nums,dp);
+        if(diff>=0){
             return true;
         }
         return false;
+
+
+
+
+
+
     }
 };
